30fd597e651a1e06b96b9d69f88903a39d9aacb6
Razum Force <greforce76@gmail.com>
2018-11-04 12:20:49 +0300

    Update to readme and text on welcome page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Time-exchange project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Frontend к Time-exchange project!
+            Frontend к проекту Time-exchange!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
