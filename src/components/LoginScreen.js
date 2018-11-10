a3b134f7e9b086d1ef4b4579998781ee783c6f36
Razum Force <greforce76@gmail.com>
2018-11-10 23:39:33 +0300

    фон, лого, кнопки на стартовой странице

---
+++ src/components/LoginScreen.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+} from "reactstrap";
+
+import './LoginScreen.scss';
+const logo = require('../static/time-exchange-logo.png');
+
+class LoginScreen extends Component {
+  render() {
+    return (
+      <div className="login-screen">
+        <Container>
+          <Row className="m-5">
+            <Col md="12">
+            <div className="login-screen__logo">
+              <img src={logo} alt="logo" />
+            </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="12">
+              <div className="login-screen__buttons">
+                <Button className="m-2 px-4 login-screen__buttons_login" size="lg">Login</Button>
+                <Button className="m-2 px-4 login-screen__buttons_register" size="lg">Register</Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default LoginScreen;
