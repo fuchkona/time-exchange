463667d11caf7830bc1957dc4ae2ce7111d2dcd3
Razum Force <greforce76@gmail.com>
2018-11-14 23:52:55 +0300

    тексты на LoginScreen согласно дизайна UI

--- src/auth/components/LoginScreen.js
+++ src/auth/components/LoginScreen.js
@@ -4,6 +4,9 @@ import {
   Row,
   Col,
   Button,
+  Card,
+  CardTitle,
+  CardText,
 } from "reactstrap";
 
 import './LoginScreen.scss';
@@ -15,11 +18,33 @@ class LoginScreen extends Component {
       <div className="login-screen">
         <Container>
           <Row className="m-5">
+            <Col md="3"></Col>
+            <Col md="6">
+              <Card body inverse className="login-screen__title-card text-center">
+                <CardTitle>
+                  Добро пожаловать в систему обмена временем
+                </CardTitle>
+              </Card>
+            </Col>
+            <Col md="3"></Col>
+          </Row>
+          <Row className="m-5">
             <Col md="12">
             <div className="login-screen__logo">
               <img src={logo} alt="logo" />
             </div>
             </Col>
+          </Row>
+          <Row className="m-5">
+            <Col md="3"></Col>
+            <Col md="6">
+              <Card body inverse className="login-screen__info-card text-center">
+                <CardText>
+                  Здесь вы можете заказать выполнение нужной вам задачи совершенно бесплатно, достаточно просто выполнить понравившуюся вам задачу другого человека!
+                </CardText>
+              </Card>
+            </Col>
+            <Col md="3"></Col>
           </Row>
           <Row>
             <Col md="12">
