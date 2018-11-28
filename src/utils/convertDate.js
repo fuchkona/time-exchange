aad7915878e88cd747938c2124173d3359f891fe
Igor Korobeynikov <igrock88@gmail.com>
2018-11-28 21:55:12 +0800

    Реализована функция для конвертация дат

---
+++ src/utils/convertDate.js
@@ -0,0 +1,12 @@
+export default function convertDate (timestamp) {
+  var a = new Date(timestamp * 1000);
+  var months = ['Янв','Фев','Мар','Апр','Май','Июнь','Июль','Авг','Сен','Окт','Ноя','Дек'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+};
